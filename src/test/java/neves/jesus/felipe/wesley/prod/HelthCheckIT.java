6c6732581894bc5fea27a05e128f7ee92a7bdd9c
Wesley Neves <wesley.neves@wexinc.com>
2021-05-25 11:22:06 -0300

    add helth check test

---
+++ src/test/java/neves/jesus/felipe/wesley/prod/HelthCheckIT.java
@@ -0,0 +1,31 @@
+package neves.jesus.felipe.wesley.prod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import junit.framework.Assert;
+
+public class HelthCheckIT {
+
+	@Test
+	public void healthCheck() throws MalformedURLException {
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.6:4444/wd/hub"), DesiredCapabilities.chrome());
+		try {
+			driver.navigate().to("http://192.168.1.6:9999/tasks");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			String version = driver.findElement(By.id("version")).getText();
+			Assert.assertTrue(version.startsWith("build"));
+		} finally {
+			driver.quit();
+		}
+	}
+
+}
